a6bf25fdc57be0d7107cd61a798d228bcba4159a
dangkhoa6599-gif <58280028+dangkhoa6599-gif@users.noreply.github.com>
2022-07-04 13:55:29 +0700

    updating

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -67,56 +67,7 @@ namespace app01.Controllers
             return RedirectToAction("Index");
         }
 
-        // GET: Employee/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.EmployeeModels == null)
-            {
-                return NotFound();
-            }
-
-            var employeeModel = await _context.EmployeeModels.FindAsync(id);
-            if (employeeModel == null)
-            {
-                return NotFound();
-            }
-            return View(employeeModel);
-        }
-
-        // POST: Employee/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,Address,Phone")] EmployeeModel employeeModel)
-        {
-            if (id != employeeModel.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(employeeModel);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EmployeeModelExists(employeeModel.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(employeeModel);
-        }
+       
 
         // GET: Employee/Delete/5
         public async Task<IActionResult> Delete(int? id)
